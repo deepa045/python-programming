3e5913c5a432ed362a52b0b9b43ff6606afd6fa6
deepa045 <37369695+deepa045@users.noreply.github.com>
2018-04-14 11:35:25 +0530

    Create input num output  a character.py

---
+++ Beginner/input num output  a character.py
@@ -0,0 +1,55 @@
+n=int(raw_input())
+if(n==1):
+  print "A"
+elif(n==2):
+    print "B"
+elif(n==3):
+  print "C"
+elif(n==4):
+  print "D"
+elif(n==5):
+  print "E"
+elif(n==6):
+  print "F"
+elif(n==7):
+  print "G"
+elif(n==8):
+  print "H"
+elif(n==9):
+  print "I"
+elif(n==10):
+  print "J"
+elif(n==11):
+  print "K"
+elif(n==12):
+  print "L"
+elif(n==13):
+  print "M"
+elif(n==14):
+  print "N"
+elif(n==15):
+  print "O"
+elif(n==16):
+  print "P"
+elif(n==17):
+  print "Q"
+elif(n==18):
+  print "R"
+elif(n==19):
+  print "S"
+elif(n==20):
+  print "T"
+elif(n==21):
+  print "U"
+elif(n==22):
+  print "V"
+elif(n==23):
+  print "W"
+elif(n==24):
+  print "X"
+elif(n==25):
+  print "Y"
+elif(n==26):
+  print "Z"
+else:
+  print "invalid"
